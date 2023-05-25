f82c766d5eadd6570a220efe210d9fdd3313b07d
chenlingzhi <chenlingzhi@ut.cn>
2023-05-25 14:39:58 +0800

    feat: 文章列表

---
+++ src/views/home/ArticleDirect.tsx
@@ -0,0 +1,34 @@
+const ArticleDirect = () =>{
+   let typecho = [
+    {
+        title:"欢迎使用 Typecho",
+        time:"15464648",
+        
+
+    }
+
+   ]
+
+   
+
+
+
+    return(
+    <div>
+        <h2>
+
+        </h2>
+        <div className="bg-gray-100 h-[100%] bg-[#EFEFEF]">
+
+        </div>
+
+        <div className="bg-gray-100 h-[100%] bg-[#EFEFEF]">
+
+        </div>
+
+    </div>)
+
+
+
+}
+export default ArticleDirect;
